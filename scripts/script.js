1e061cf1816be1aa5088619cdfde17ff06b1a2f0
Katy Chan <cchan@my.bcit.ca>
2023-03-27 11:20:45 -0700

    more button

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,70 @@
 
 // button
-const buttonRef = document.querySelector("button");
+// const buttonRef = document.querySelector("button");
 // console.log(buttonRef);
 
-function alertUser()
-{
-    alert("You clicked!!!");
-    // remove, so alert only appear once
-    // buttonRef.removeEventListener("click", alertUser);
+// function alertUser()
+// {
+//     alert("You clicked!!!");
+//     // remove, so alert only appear once
+//     // buttonRef.removeEventListener("click", alertUser);
  
-};
+// };
 
-buttonRef.addEventListener("click", alertUser, {once:true});
-
-
-function changeBGPink()
-{
-    // document.body.style.backgroundColor = "pink";
-    document.body.classList.add("pingBG");
-};
+// buttonRef.addEventListener("click", alertUser, {once:true});
 
 
-buttonRef.addEventListener("click", changeBGPink);
+// function changeBGPink()
+// {
+//     // document.body.style.backgroundColor = "pink";
+//     document.body.classList.add("pingBG");
+// };
 
 
-function changeText()
-{
-    if (buttonRef.textContent === "Click Me!") {
-        buttonRef.textContent = "Clicked!" 
-        // one equal sogn just assigning, === is for equal check
-    }else {
-        buttonRef.textContent = "Click Me!";
+// buttonRef.addEventListener("click", changeBGPink);
+
+
+// function changeText()
+// {
+//     if (buttonRef.textContent === "Click Me!") {
+//         buttonRef.textContent = "Clicked!" 
+//         // one equal sogn just assigning, === is for equal check
+//     }else {
+//         buttonRef.textContent = "Click Me!";
+//     };
+// };
+
+// buttonRef.addEventListener("click", changeText);
+
+
+// function updateImage(event) {
+//     console.log(event);
+//     const image = document.querySelector("#shoppingCart");
+//     image.setAttribute("src", "images/shoppingCart.png");
+//     // think we are in html, no need ..
+//     image.setAttribute("alt","shopping cart");
+//     image.setAttribute("width",50);
+//     image.setAttribute("height",50);
+
+// };
+
+// buttonRef.addEventListener("click", updateImage);
+
+
+
+
+// new
+const buttonContainer = document.querySelector(".button-container")
+// console.log(buttonContainer);
+
+// event parameter auto receives event object
+function bgChange(event){
+    // console.log("buttons clicked", event.target);
+    console.log("buttons clicked", event.target.tagName);
+    if (event.target.tagName === "BUTTON") {
+        event.target.classList.add("greenBG");
     };
 };
 
-buttonRef.addEventListener("click", changeText);
-
-
-function updateImage() {
-    const image = document.querySelector("#shoppingCart");
-    image.setAttribute("src", "images/shoppingCart.png");
-    // think we are in html, no need ..
-    image.setAttribute("alt","shopping cart");
-    image.setAttribute("width",50);
-    image.setAttribute("height",50);
-
-};
-
-buttonRef.addEventListener("click", updateImage);
+buttonContainer.addEventListener("mouseover", bgChange);
+// ("mouseover", (event))
