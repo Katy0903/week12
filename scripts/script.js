83736139dfa77a253cc16efc30286c58d93a9a14
Katy Chan <cchan@my.bcit.ca>
2023-03-27 10:32:10 -0700

    add button

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 
+// button
+const buttonRef = document.querySelector("button");
+
+function alertUser()
+{
+    alert("You clicked!!!");
+    // remove, so alert only appear once
+    // buttonRef.removeEventListener("click", alertUser);
+ 
+};
+
+buttonRef.addEventListener("click", alertUser, {once:true});
+
+
+// function changeColor()
+// {
+
+
+// };
+
+
